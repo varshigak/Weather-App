331963cf9c0ae2d9c55cf9654e19b1a52b69dc3e
Varshiga Kandeepan <142120820+varshigak@users.noreply.github.com>
2023-08-16 00:07:20 -0400

    Update script.js
    Fixed a spacing issue.

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ let weather = {
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed: "  + speed + "km/h";
+        document.querySelector(".wind").innerText = "Wind speed: "  + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900?" + name + "')";
     },
